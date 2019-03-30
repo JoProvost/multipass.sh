f1ca30315dcb000caa00c5fd96a6f6171cb9f0d2
Jonathan Provost <jprovost.sh@gmail.com>
2019-03-30 13:25:05 -0400

    Show stack trace on test fail
    Also, common usage path of `shell.sh` are tested with hash and encoding.

--- test/functions.sh
+++ test/functions.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-assert_error=
-
 run_test_suite() {
   errors=0
   echo "$0"
   for test in $( typeset -F | cut -d ' ' -f 3 | grep ^test_ ); do
+    standard_errors=$(tempfile)
     (
       echo -n "  ${test}"
-      if ${test}; then
+      if ${test} 2>${standard_errors} && [ "$(wc -l < ${standard_errors})" -eq 0 ]; then
         echo " ✔"
       else
         echo " 😭"
-        echo "    ${assert_error:-unknown error}"
+        sed 's/^/   /g' ${standard_errors} >&2
         exit 1
       fi
     )
     errors=$(( errors + $? ))
+    rm ${standard_errors}
   done
+  echo ""
   exit $errors
 }
 
 assert_that() {
   if ! test "$@"; then
-    assert_error="assert_that $*"
+    error assert_that "$@"
     return 1
   fi
+}
+
+readonly project_root=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]}))/../)
+error() {
+  echo "> Error: $@" >&2
+  if [ ${#FUNCNAME[@]} -gt 2 ]; then
+    echo "  Call stack:"
+    for ((i=1;i<${#FUNCNAME[@]}-1;i++)); do
+      echo "    at ${FUNCNAME[$i]}($(find ${project_root} -name $(basename ${BASH_SOURCE[$i+1]})):${BASH_LINENO[$i]})"
+    done
+  fi >&2
+  echo "" >&2
 }
--- test/shell_test.sh
+++ test/shell_test.sh
@@ -5,13 +5,80 @@ cd $(dirname $(readlink -f ${BASH_SOURCE[0]}))
 . ../lib/shell.sh
 cd - >/dev/null
 
-test_generating_a_password_with_all_setings_from_command_line() {
-  load() { :; }
-  save() { :; }
-  secret() { eval $1='"master_password"'; }
-  type_password() { echo "$1"; }
+propose() { :; }
+ask() { :; }
+load() { :; }
+save() { :; }
+secret() { eval $1='"master_password"'; }
+type_password() { echo "$1"; }
+web_site() { :; }
 
-  assert_that "$( pass "site" "4" "none" "s/[^0-9a-zA-Z]//g" )" = "LpzFqyeTVIhpglEIaxHaZrGRIw"
+
+test_generating_a_password_with_all_setings_from_command_line() {
+  assert_that "$( pass "site_name" "4" "none" "s/[^0-9a-zA-Z]//g" )" = "YWP1L6hVyJRq5noiQEqnrsOh4U"
 }
+
+test_generating_a_password_will_ask_all_setting_to_the_user() {
+  ask() {
+    case $1 in
+      site)       site='site' ;;
+      iterations) iterations='4' ;;
+      length)     length='none' ;;
+      filter)     filter='s/[^0-9a-zA-Z]//g' ;;
+    esac
+  }
+
+  assert_that "$( pass )" = "LpzFqyeTVIhpglEIaxHaZrGRIw"
+}
+
+test_generating_a_password_asking_for_site_and_loading_the_rest_from_vault() {
+  load() {
+    case $1 in
+      'site_name')
+        iterations='4'
+        length='none'
+        filter='s/[^0-9a-zA-Z]//g'
+      ;;
+    esac
+  }
+  ask() {
+    case $1 in
+      site) site='site_name' ;;
+    esac
+  }
+
+  assert_that "$( pass )" = "YWP1L6hVyJRq5noiQEqnrsOh4U"
+}
+
+test_generating_a_password_proposes_a_list_of_known_sites() {
+  web_site() { echo "web_site_1"; }
+  list() { echo "known_site_1"; echo "known_site_2"; }
+
+  propose() {
+    local key="${1:-}"
+    local text="${2:-}"
+    local default="${3:-}"
+    local list="${4:-}"
+
+    assert_that "$key" = "site"
+    assert_that "$default" = "$( echo "web_site_1" )"
+    assert_that "$(cat $list)" = "$( echo "known_site_1"; echo "known_site_2" )"
+
+    site='known_site_1'
+  }
+
+  load() {
+    case $1 in
+      'known_site_1')
+        iterations='4'
+        length='none'
+        filter='s/[^0-9a-zA-Z]//g'
+      ;;
+    esac
+  }
+
+  assert_that "$( pass )" = "DAQLSJdqWtglhXqoQnMiwTnR4x4"
+}
+
 
 run_test_suite
