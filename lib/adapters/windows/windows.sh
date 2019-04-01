a07ab6c8731b967e05f5ae5ab58544a45c5fe48c
Mathieu Mitchell <mat128@gmail.com>
2019-04-01 18:05:30 -0400

    Ensure /proc/version exists before using it
    During test for cygwin/wsl, /proc/version is read to verify the platform
    
    Ensure file exists before read it.

--- lib/adapters/windows/windows.sh
+++ lib/adapters/windows/windows.sh
@@ -2,6 +2,7 @@
 
 # All executables are win-executables
 use_cygwin() {
+  test -f /proc/version || return 1
   grep -qE "(CYGWIN_NT|Cygwin)" /proc/version && return 0 || return 1
 }
 
@@ -9,6 +10,7 @@ use_cygwin() {
 # https://en.wikipedia.org/wiki/Binfmt_misc
 # sudo echo ":WSLInterop:M::MZ::/init:" > /proc/sys/fs/binfmt_misc/register
 use_wsl() {
+  test -f /proc/version || return 1
   grep -qE "(Microsoft|WSL)" /proc/version && return 0 || return 1
 }
 
