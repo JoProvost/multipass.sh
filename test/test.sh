8f0b2562947ca6c4bc730a0712d9b0c69a3f753d
Jonathan Provost <jprovost.sh@gmail.com>
2019-04-01 17:12:12 -0400

    Avoid shell expansion of `*_test.sh` when loading tests
    The idea is to let `find` find the files matching the name pattern.  By
    running `test.sh` in its own directory, bash was expanding `*_test.sh`,
    thus giving unwanted parrameters to `find`.

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -x
 errors=0
-for suite in $(find $(dirname $0) -name *_test.sh); do
+for suite in $(find $(dirname $0) -name '*_test.sh'); do
   $suite
   errors=$(( errors + $? ))
 done
