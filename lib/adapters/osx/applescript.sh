34d11f5d9e05c0fbfc059672219ca0af42e5768b
Mathieu Mitchell <mat128@gmail.com>
2019-04-01 17:32:45 -0400

    Introduce OSX-compatible adapter
    Leverage AppleScript to implement an OSX-compatible adapter.

---
+++ lib/adapters/osx/applescript.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+_applescript_type_password() {
+  osascript -e "tell application \"System Events\"
+keystroke \"$1\"
+end tell"
+}
+
+_use_applescript() {
+  which osascript >/dev/null && return 0 || return 1
+}
+
+_applescript_ask() {
+  local key="${1:-}"
+  local text="${2:-}"
+  local default="${3:-}"
+
+  if ! [ "${!key:-}" ]; then
+    local value="$( osascript <<EOF | sed 's#.*text returned:##g'
+    display dialog "$text" default answer "$default" with icon stop buttons {"Cancel", "Continue"} default button "Continue"
+EOF
+)"
+    [ -n "$value" ]
+    eval $key='"$value"'
+  fi
+}
+
+_applescript_secret() {
+  local key="${1:-}"
+  local text="${2:-}"
+
+    local value="$( osascript <<EOF | sed 's#.*text returned:##g'
+    display dialog "$text" with icon stop buttons {"Cancel", "Continue"} default button "Continue" with hidden answer
+EOF
+)"
+  [ -n "$value" ]
+  eval $key='"$value"'
+}
+
+if _use_applescript; then
+  type_password() { _applescript_type_password "$@"; }
+  ask() { _applescript_ask "$@"; }
+  secret() { _applescript_secret "$@"; }
+fi
