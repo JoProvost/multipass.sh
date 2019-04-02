48403a77bc93483c33e5f385d081a1e65c6304aa
Jonathan Provost <jprovost.sh@gmail.com>
2019-04-02 11:55:47 -0400

    Rename ADB pipe and reader concept to spool and spooler
    The idea at first was to pipe operations in a file and have a reader
    running in ADB executing the operation.  Now, as all operations are
    in a file and all files are executed in order in a folder, this looks
    more like a spool.

--- lib/adapters/android/adb-shell.sh
+++ lib/adapters/android/adb-shell.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
-ADB_PIPE=${ADB_PIPE:-/storage/self/primary/.adb-reader.pipe}
-ADB_PIPE_READER=${ADB_PIPE_READER:-$(dirname $ADB_PIPE)/adb-reader.sh}
+ADB_SPOOL=${ADB_SPOOL:-/storage/self/primary/.adb-spool}
+ADB_SPOOLER=${ADB_SPOOLER:-$(dirname $ADB_SPOOL)/adb-spooler.sh}
 
 ADB_COMMAND_COUNTER=0
 
 adb_shell() {
-  mkdir -p $ADB_PIPE
+  mkdir -p $ADB_SPOOL
   local command=''
   for i in "$@"; do
     i="${i//\\/\\\\}"
     command="$command \"${i//\"/\\\"}\""
   done
-  echo "$command" > $ADB_PIPE/$(date +%s).$(( ADB_COMMAND_COUNTER++ ))
+  echo "$command" > $ADB_SPOOL/$(date +%s).$(( ADB_COMMAND_COUNTER++ ))
 }
 
-_adb_shell_reader_install() {
-  if ! [ -f $ADB_PIPE_READER ]; then
-    mkdir -p $(dirname $ADB_PIPE_READER)
+_adb_shell_spooler_install() {
+  if ! [ -f $ADB_SPOOLER ]; then
+    mkdir -p $(dirname $ADB_SPOOLER)
 
-    cat > $ADB_PIPE_READER << ____EOF
+    cat > $ADB_SPOOLER << ____EOF
 #!/system/bin/sh
 echo \$0 is running!
 echo You can safely quit ADB by doing [Ctrl] + [C] and disconnect your phone.
 while true
 do
-  for f in ${ADB_PIPE}/*; do
-    if [ ! -f \$f ]; then
+  for command in ${ADB_SPOOL}/*; do
+    if [ ! -f \$command ]; then
       sleep 1
     else
-      sh \$f
-      rm \$f
+      sh \$command
+      rm \$command
     fi
   done
 done </dev/null >/dev/null 2>&1 &
 ____EOF
 
     echo "Type the following on your PC at each boot:"
-    echo "  adb shell sh $ADB_PIPE_READER"
+    echo "  adb shell sh $ADB_SPOOLER"
   fi
 }
 
 use_adb_shell() {
-  [ -d $(dirname $ADB_PIPE) ] && return 0 || return 1
+  [ -d $(dirname ${ADB_SPOOL}) ] && return 0 || return 1
 }
 
 if use_adb_shell; then
-  _adb_shell_reader_install
+  _adb_shell_spooler_install
 fi
